432fbe321a3108df9e8898d155f69291e23dac1b
Meet Gandhi <85786711+meetgandhi586@users.noreply.github.com>
2023-06-22 15:22:47 +0530

    Updated.

---
+++ program1.py
@@ -0,0 +1,19 @@
+'''
+Write a python Program to print the following string in a specific format.
+Write a Python program to print the following string in a specific format (see the output).
+Sample String : 
+"Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+Output :
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+'''
+#\n - Space 
+#\t - Tabular Space 
+#print() - Prints Statement 
+
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are"
+)
