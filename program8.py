296cfd0f2ebd9fb5f06b5bef26218ca5fbe45c58
Meet Gandhi <85786711+meetgandhi586@users.noreply.github.com>
2023-06-22 16:08:23 +0530

    Updated.

---
+++ program8.py
@@ -0,0 +1,11 @@
+'''
+Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+'''
+#color_list variable for storing list 
+color_list = ["Red","Green","White" ,"Black"]
+#printing the first and last name from list
+print("%s %s"%(color_list[0],color_list[-1]))
+#s for string
+#0 for first element
+#-1 for last element
