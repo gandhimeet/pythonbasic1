2f0217b36d669d099d12592725c617dbf6f3dde3
Meet Gandhi <85786711+meetgandhi586@users.noreply.github.com>
2023-06-22 15:23:52 +0530

    Updated.

---
+++ program2.py
@@ -0,0 +1,13 @@
+'''
+Write a Python program to find out what version of Python you are using.
+'''
+
+#sys - system module
+#import keyword for importing module 
+import sys
+print("Python Version")
+#sys.version - system version
+print(sys.version)
+print("Python Version INFO")
+#sys.version_info - system information 
+print(sys.version_info)
