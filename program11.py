6da1b6aa000b72346076c4404b9fe3796ae8214c
Meet Gandhi <85786711+gandhimeet@users.noreply.github.com>
2023-06-23 10:51:51 +0530

    Updated.

---
+++ program11.py
@@ -0,0 +1,10 @@
+'''
+Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+Sample function : abs()
+Expected Result :
+abs(number) -> number
+Return the absolute value of the argument.
+'''
+#abs() - absoulte built in function of python 
+print(abs.__doc__)
+#__doc__ -  used for the documentation of abs()
