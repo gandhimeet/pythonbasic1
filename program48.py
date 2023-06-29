2ccaba8f99373887e65d0f52c08f76079529b258
Meet Gandhi <85786711+gandhimeet@users.noreply.github.com>
2023-06-29 12:19:28 +0530

    Updated.

---
+++ program48.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to parse a string to float or integer.
+'''
+#inputing n value 
+n = input("Enter a Float Value:")
+#floating n 
+print(float(n))
+#integer n
+print(int(float(n)))
