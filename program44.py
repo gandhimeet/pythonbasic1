7add866542ec54bc37756066888f2351da028d22
Meet Gandhi <85786711+gandhimeet@users.noreply.github.com>
2023-06-29 12:04:17 +0530

    Updated.

---
+++ program44.py
@@ -0,0 +1,7 @@
+'''
+Write a Python program to locate Python site packages.
+'''
+#importing site module 
+import site
+#site.getsitepackages() gives python package path or location
+print(site.getsitepackages())
