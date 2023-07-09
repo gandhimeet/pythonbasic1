fc534dd96a407b10b39b0a3a87342519bdfb8b29
Meet Gandhi <85786711+gandhimeet@users.noreply.github.com>
2023-07-09 17:12:33 +0530

    Updated.

---
+++ program58.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to sum the first n positive integers.
+'''
+#Taking Input for number 
+n = int(input("Input a number: "))
+#sum_num for storing sum of first n positive integers
+sum_num = (n * (n + 1)) / 2
+#displaying the positive sum of positive integers
+print("Sum of the first", n ,"positive integers:", sum_num)
