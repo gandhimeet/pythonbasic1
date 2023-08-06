0cf25891d1f44087a1290577e5895a12310193c1
Meet Gandhi <85786711+gandhimeet@users.noreply.github.com>
2023-08-06 14:35:26 +0530

    Updated.

---
+++ program82.py
@@ -0,0 +1,6 @@
+'''
+Write a Python program to concatenate N strings.
+'''
+s = sum([10,20,30])
+print("\nSum of the container: ", s)
+print()
---
+++ program87.py
@@ -0,0 +1,7 @@
+'''
+Write a Python program to get the size of a file.
+'''
+import os
+file_size = os.path.getsize("program87.py")
+print("\nThe size of program85.py is :",file_size,"Bytes")
+print()
