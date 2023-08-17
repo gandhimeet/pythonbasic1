5ed2512e4b428b4dbffd2d0f2b9ac0ccd4262217
Meet Gandhi <85786711+gandhimeet@users.noreply.github.com>
2023-08-17 06:34:50 +0530

    Updated.

---
+++ program113.py
@@ -0,0 +1,10 @@
+"""
+Write a Python program that inputs a number and generates an error message if it is not a number.
+"""
+while True:
+    try:
+        a = int(input("Input a number: "))
+        break
+    except ValueError:
+        print("\nThis is not a number. Try again...")
+        print()
